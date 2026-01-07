940bc7d2a968e16d9516b7a61a2441d83b35c2a2
wenhun <wenhun1@gmail.com>
2026-01-07 14:31:56 +0200

    add apps to settings

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "social_media",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
